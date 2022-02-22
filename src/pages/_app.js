56b7e945e4d9e3297669f02b86dc549da67963e3
Emmanuel Eboh <63825997+EOEboh@users.noreply.github.com>
2022-02-22 10:40:49 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,11 @@
 /** Uncomment the below codeblock if you want to add google analytics for more info please visit our docs analytics section */
 /** 
 
-import Head from 'next/head'
 import Router from 'next/router';
 import { initGA, logPageView } from 'analytics';
 */
+
+import Head from 'next/head'
 import { useEffect } from 'react';
 import 'rc-drawer/assets/index.css';
 import 'assets/css/react-slick.css';
